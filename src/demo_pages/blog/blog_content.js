895c1d400712bb61ebea55095057374504c1dcb8
Mr. Radd <conrad.horton@gmail.com>
2025-05-13 16:46:50 -0700

    Bug fix.

--- src/demo_pages/blog/blog_content.js
+++ src/demo_pages/blog/blog_content.js
@@ -1,12 +1,12 @@
 const BLOG_CONTENT = {
-  para1: `This page is generated dynamically using Vanilla JavaScript. The text and image you are seeing is loaded from a JS object that is being looped over. On each iteration the property is turned into either a <p> or a <img> depending on how the property is named.
-  For instance to load a paragraph the property must have the word 'para' in it. To load an image the property's name must have 'img' in it. In these cases the properties are called para1, img1, and para2.`,
-  img1: {
+  _para_1: `This page is generated dynamically using Vanilla JavaScript. The text and image you are seeing is loaded from a JS object that is being looped over. On each iteration the property is turned into either a <p> or a <img> depending on how the property is named.
+  For instance to load a paragraph the property must have the word '_para_' in it. To load an image the property's name must have '_img_' in it. In these cases the properties are called '_para_1', '_img_1', and '_para_2'.`,
+  _img_1: {
     url: "../../assets/i_got_this.png",
     altText: "RAMBO!!!",
     caption: "Thumbs up Rambo!",
   },
-  para2: `This is para2 that is being loaded here. For this page the JavaScript is appending child elements to the div with the ID of 'content_area' Since I am using string literals to display this text, I have the ability to make my own breks in the paragraph.
+  _para_2: `This is '_para_2' that is being loaded here. For this page the JavaScript is appending child elements to the div with the ID of 'content_area' Since I am using string literals to display this text, I have the ability to make my own breks in the paragraph.
   
   Like this.`,
 };
